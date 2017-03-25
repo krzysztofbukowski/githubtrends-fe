f7fd03358dae584fa57df931e6fcf30e57dae66d
Krzysztof Bukowski <ja@krzysztofbukowski.pl>
2017-03-25 18:53:40 +0000

    Added repositories API service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule}  from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule}    from '@angular/http';
 import {AppComponent} from './app.component';
 import {ConfigManager} from './config/config-manager';
 import {DevelopmentConfig} from './config/development.config';
 import {TestingConfig} from './config/testing.config';
 import {ProductionConfig} from './config/production.config';
 import {CompareRepositoriesComponent} from './compare-repositories/compare-repositories.component';
+import {RepositoriesService} from './api/repositories.service';
 
 
 let configManager: ConfigManager = new ConfigManager();
@@ -19,7 +21,8 @@ declare var process: any;
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        HttpModule
     ],
     declarations: [
         AppComponent,
@@ -28,7 +31,9 @@ declare var process: any;
     providers: [{
         provide: 'appConfig',
         useValue: configManager.getConfig(process.env.ENV)
-    }],
+    },
+        RepositoriesService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
